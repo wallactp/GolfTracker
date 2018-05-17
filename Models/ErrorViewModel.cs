6a627b450df206164cbca3b1b5444e7f9dfbe9c4
Tyler Wallace <tyler.wallace@icfolson.com>
2018-05-17 10:22:43 -0400

    Adding dotnet core with identity

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GolfTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
